cd8e0973b8c76e8375d1e994a87eb456e55dc16a
viktolim <83975054+viktolim@users.noreply.github.com>
2022-02-12 21:24:32 +0200

    Šifruotas kodas

---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, EventArgs e)
+        {
+            int n = 31;
+            string M = SetText.Text;
+            char m;
+            int mSize =SetText.Text.Length;
+            SetText.Text = null;
+            int ArrayLoc;
+            char[] array = new char[] { 'a','ą','b','c','č','d','e','ę','ė','f','g','h','i','į','y','j','k','l','m','n','o','p','r','s','š','t','u','ų','ū','v','z','ž'};
+            for(int i=0; i<mSize; i++)
+            {
+                m = M[i];
+                for(int j=0; j<n; j++)
+                {
+                    if (array[j] == m)
+                    {
+                        ArrayLoc = int.Parse(kSet.Value.ToString());
+                        if(j+ArrayLoc > n)
+                        {
+                            SetText.Text = SetText.Text + array[((j + ArrayLoc)-n)-1];
+                        }
+                        else
+                        {
+                            SetText.Text = SetText.Text + array[j + ArrayLoc];
+                        }
+
+                    }
+                }
+            }
+        }
+    }
+}
